5bbfced281c3289ee8ee0749629aa152b02cd92a
Aung Htoo Khine <70278608+aunghtookhine@users.noreply.github.com>
2023-08-20 22:04:22 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,99 @@
+var inputDayTag = document.querySelector(".inputDay");
+var inputMonthTag = document.querySelector(".inputMonth");
+var inputYearTag = document.querySelector(".inputYear");
+var calculateBtnTag = document.querySelector(".calculateBtn");
+var resultYearTag = document.querySelector(".resultYear");
+var resultMonthTag = document.querySelector(".resultMonth");
+var resultDayTag = document.querySelector(".resultDay");
+var errorDayTag = document.querySelector(".errorDay");
+var errorMonthTag = document.querySelector(".errorMonth");
+var errorYearTag = document.querySelector(".errorYear");
+var dayLabelTag = document.querySelector(".dayLabel");
+var monthLabelTag = document.querySelector(".monthLabel");
+var yearLabelTag = document.querySelector(".yearLabel");
+var months = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+};
+var checkLeapYear = function (birthYear) {
+    if ((birthYear % 4 === 0 && birthYear % 100 !== 0) || birthYear % 400 === 0) {
+        months[2] = 29;
+    }
+    else {
+        months[2] = 28;
+    }
+};
+var checkValidDate = function (birthDay, birthMonth, birthYear, months, year) {
+    var i = true;
+    errorDayTag.innerHTML = "";
+    inputDayTag.style.border = "1px solid hsl(0, 1%, 44%)";
+    dayLabelTag.style.color = "black";
+    errorMonthTag.innerHTML = "";
+    errorYearTag.innerHTML = "";
+    if (birthDay > months[birthMonth] || birthDay > 31 || isNaN(birthDay)) {
+        inputDayTag.style.border = "1px solid #FF0000";
+        dayLabelTag.style.color = "red";
+        errorDayTag.innerHTML = "Must be a valid day";
+        i = false;
+    }
+    if (birthMonth > 12 || isNaN(birthMonth)) {
+        inputMonthTag.style.border = "1px solid #FF0000";
+        monthLabelTag.style.color = "red";
+        errorMonthTag.innerHTML = "Must be a valid month";
+        i = false;
+    }
+    if (birthYear > year || isNaN(birthYear)) {
+        inputYearTag.style.border = "1px solid #FF0000";
+        yearLabelTag.style.color = "red";
+        errorYearTag.innerHTML = "Must be a valid month";
+        i = false;
+    }
+    return i;
+};
+// const checkEmpty = (
+//   birthDay: number,
+//   birthMonth: number,
+//   birthYear: number
+// ) => {
+//   if (isNaN(birthDay)) {
+//   }
+// };
+calculateBtnTag === null || calculateBtnTag === void 0 ? void 0 : calculateBtnTag.addEventListener("click", function () {
+    var birthDay = parseInt(inputDayTag.value);
+    var birthMonth = parseInt(inputMonthTag.value);
+    var birthYear = parseInt(inputYearTag.value);
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    checkLeapYear(birthYear);
+    if (checkValidDate(birthDay, birthMonth, birthYear, months, year)) {
+        resultDayTag.innerHTML = "";
+        resultMonthTag.innerHTML = "";
+        resultYearTag.innerHTML = "";
+        if (birthDay > day) {
+            day += months[month - 1];
+            month -= 1;
+        }
+        if (birthMonth > month) {
+            month += 12;
+            year -= 1;
+        }
+        var resultDay = day - birthDay;
+        var resultMonth = month - birthMonth;
+        var resultYear = year - birthYear;
+        resultDayTag.append(resultDay.toString());
+        resultMonthTag.append(resultMonth.toString());
+        resultYearTag.append(resultYear.toString());
+    }
+});
---
+++ app.ts
@@ -0,0 +1,126 @@
+const inputDayTag = document.querySelector(".inputDay") as HTMLInputElement;
+const inputMonthTag = document.querySelector(".inputMonth") as HTMLInputElement;
+const inputYearTag = document.querySelector(".inputYear") as HTMLInputElement;
+const calculateBtnTag = document.querySelector(
+  ".calculateBtn"
+) as HTMLButtonElement;
+
+const resultYearTag = document.querySelector(".resultYear") as HTMLSpanElement;
+const resultMonthTag = document.querySelector(
+  ".resultMonth"
+) as HTMLSpanElement;
+const resultDayTag = document.querySelector(".resultDay") as HTMLSpanElement;
+
+const errorDayTag = document.querySelector(".errorDay") as HTMLSpanElement;
+const errorMonthTag = document.querySelector(".errorMonth") as HTMLSpanElement;
+const errorYearTag = document.querySelector(".errorYear") as HTMLSpanElement;
+
+const dayLabelTag = document.querySelector(".dayLabel") as HTMLLabelElement;
+const monthLabelTag = document.querySelector(".monthLabel") as HTMLLabelElement;
+const yearLabelTag = document.querySelector(".yearLabel") as HTMLLabelElement;
+
+var months = {
+  1: 31,
+  2: 28,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31,
+};
+
+const checkLeapYear = (birthYear: number) => {
+  if ((birthYear % 4 === 0 && birthYear % 100 !== 0) || birthYear % 400 === 0) {
+    months[2] = 29;
+  } else {
+    months[2] = 28;
+  }
+};
+
+const checkValidDate = (
+  birthDay: number,
+  birthMonth: number,
+  birthYear: number,
+  months: object,
+  year: number
+) => {
+  var i = true;
+  errorDayTag.innerHTML = "";
+  inputDayTag.style.border = "1px solid hsl(0, 1%, 44%)";
+  dayLabelTag.style.color = "black";
+
+  errorMonthTag.innerHTML = "";
+  errorYearTag.innerHTML = "";
+
+  if (birthDay > months[birthMonth] || birthDay > 31 || isNaN(birthDay)) {
+    inputDayTag.style.border = "1px solid #FF0000";
+    dayLabelTag.style.color = "red";
+    errorDayTag.innerHTML = "Must be a valid day";
+    i = false;
+  }
+  if (birthMonth > 12 || isNaN(birthMonth)) {
+    inputMonthTag.style.border = "1px solid #FF0000";
+    monthLabelTag.style.color = "red";
+    errorMonthTag.innerHTML = "Must be a valid month";
+    i = false;
+  }
+  if (birthYear > year || isNaN(birthYear)) {
+    inputYearTag.style.border = "1px solid #FF0000";
+    yearLabelTag.style.color = "red";
+    errorYearTag.innerHTML = "Must be a valid month";
+    i = false;
+  }
+
+  return i;
+};
+
+// const checkEmpty = (
+//   birthDay: number,
+//   birthMonth: number,
+//   birthYear: number
+// ) => {
+//   if (isNaN(birthDay)) {
+
+//   }
+// };
+
+calculateBtnTag?.addEventListener("click", () => {
+  const birthDay = parseInt(inputDayTag.value);
+  const birthMonth = parseInt(inputMonthTag.value);
+  const birthYear = parseInt(inputYearTag.value);
+
+  const date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+
+  checkLeapYear(birthYear);
+  if (checkValidDate(birthDay, birthMonth, birthYear, months, year)) {
+    resultDayTag.innerHTML = "";
+    resultMonthTag.innerHTML = "";
+    resultYearTag.innerHTML = "";
+
+    if (birthDay > day) {
+      day += months[month - 1];
+      month -= 1;
+    }
+
+    if (birthMonth > month) {
+      month += 12;
+      year -= 1;
+    }
+
+    const resultDay = day - birthDay;
+    const resultMonth = month - birthMonth;
+    const resultYear = year - birthYear;
+
+    resultDayTag.append(resultDay.toString());
+    resultMonthTag.append(resultMonth.toString());
+    resultYearTag.append(resultYear.toString());
+  }
+});
